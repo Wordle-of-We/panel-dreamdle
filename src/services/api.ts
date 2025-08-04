e42ffb5672c55fee379f016464dbf598c142c324
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-08-03 21:15:07 -0300

    feature(characters): atualizar estrutura de dados para o campo 'paper' e ajustar manipulação no modal

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, '') || 'http://localhost:3000';
+const API_BASE_URL = 'http://localhost:3000';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL +'/api',
   withCredentials: true,
 });
 
